d15ef9a6afaac527c2f0743b09ecc2ff6899d63a
Le Duy <duylangtu931@gmail.com>
2024-03-09 00:26:51 +0700

    feat:implement refreshToken services

--- src/common/utils/crypto.ts
+++ src/common/utils/crypto.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken"
 import bcrypt from "bcryptjs"
+import crypto from "crypto"
 
 export const generateToken = (payload: string | object | Buffer) => {
     return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
@@ -16,4 +17,41 @@ export const hashPassword = async (password: string) => {
 
 export const comparePassword = async (password: string, hash: string) => {
     return await bcrypt.compare(password, hash)
+}
+
+export const createTokenPair = async (
+    payload: string | object | Buffer,
+    publicKey: string,
+    privateKey: string
+): Promise<{ accessToken: string; refreshToken: string }> => {
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, publicKey, { expiresIn: "2 days" }, (err, accessToken) => {
+            if (accessToken == undefined || err) {
+                reject(err)
+                throw new Error("Failed to create access token")
+            }
+            jwt.sign(payload, privateKey, { expiresIn: "7 days" }, (err, refreshToken) => {
+                if (refreshToken == undefined || err) {
+                    reject(err)
+                    throw new Error("Failed to create refresh token")
+                }
+                resolve({ accessToken, refreshToken })
+            })
+        })
+    })
+    // const accessToken = jwt.sign(payload, publicKey, {
+    //     expiresIn: "2 days"
+    // })
+    // const refreshToken = jwt.sign(payload, privateKey, {
+    //     expiresIn: "7 days"
+    // })
+
+    // return { accessToken, refreshToken }
+}
+
+export const createKeyPair = async () => {
+    const publicKey = await crypto.randomBytes(64).toString("hex")
+    const privateKey = await crypto.randomBytes(64).toString("hex")
+
+    return { publicKey, privateKey }
 }
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,12 @@
 import "express"
 import { TFunction } from "i18next"
+import { IKeyToken } from "@/interface/schema/IKeyToken"
 
 declare module "express" {
     export interface Request {
         userId: string
         role: string
+        keyToken: IKeyToken
         t: TFunction<"translation", undefined>
     }
 }
