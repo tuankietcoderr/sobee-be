9c30e09785edd83e702d4c54600fead63c26a665
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2024-05-14 01:24:26 +0700

    feat: add attribute service (#20)

--- src/controller/attribute/attribute.controller.ts
+++ src/controller/attribute/attribute.controller.ts
@@ -1,7 +1,7 @@
 import { IAttribute, IRoute } from "@/interface"
 import { AttributeService } from "./attribute.service"
 import { Request, Response, Router } from "express"
-import { ErrorResponse, HttpStatusCode, SuccessfulResponse } from "@/common/utils"
+import { ErrorResponse, HttpStatusCode, SuccessfulResponse, asyncHandler } from "@/common/utils"
 import middleware from "@/common/middleware"
 import { ERole } from "@/enum"
 
@@ -27,68 +27,48 @@ export class AttributeController implements IRoute {
             this.PATHS.ROOT,
             middleware.verifyToken,
             middleware.verifyRoles(ERole.ADMIN, ERole.STAFF),
-            middleware.mustHaveFields<IAttribute>("name", "description"),
-            this.createAttribute
+            middleware.mustHaveFields<IAttribute>("name"),
+            asyncHandler(this.createAttribute)
         )
         this.router.put(
             this.PATHS.ATTRIBUTE,
             middleware.verifyToken,
             middleware.verifyRoles(ERole.ADMIN, ERole.STAFF),
-            this.updateAttribute
+            asyncHandler(this.updateAttribute)
         )
         this.router.delete(
             this.PATHS.ATTRIBUTE,
             middleware.verifyToken,
             middleware.verifyRoles(ERole.ADMIN, ERole.STAFF),
-            this.deleteAttribute
+            asyncHandler(this.deleteAttribute)
         )
-        this.router.get(this.PATHS.ROOT, this.getAllAttribute)
-        this.router.get(this.PATHS.ATTRIBUTE, this.getAttribute)
+        this.router.get(this.PATHS.ROOT, asyncHandler(this.getAllAttribute))
+        this.router.get(this.PATHS.ATTRIBUTE, asyncHandler(this.getAttribute))
     }
 
     private async createAttribute(req: Request, res: Response) {
-        try {
-            const attribute = await AttributeController.attributeService.create(req.body)
-            new SuccessfulResponse(attribute, HttpStatusCode.CREATED, "Attribute created succesfully!").from(res)
-        } catch (e: any) {
-            new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.message).from(res)
-        }
+        const attribute = await AttributeController.attributeService.create(req.body)
+        new SuccessfulResponse(attribute, HttpStatusCode.CREATED, "Attribute created succesfully!").from(res)
     }
 
     private async updateAttribute(req: Request, res: Response) {
-        try {
-            const attribute = await AttributeController.attributeService.update(req.params.attributeId, req.body)
-            new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute updated succesfully!").from(res)
-        } catch (e: any) {
-            new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.message).from(res)
-        }
+        const attribute = await AttributeController.attributeService.update(req.params.attributeId, req.body)
+        new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute updated succesfully!").from(res)
     }
 
     private async deleteAttribute(req: Request, res: Response) {
-        try {
-            const attribute = await AttributeController.attributeService.delete(req.params.attributeId)
-            new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute deleted succesfully!").from(res)
-        } catch (e: any) {
-            new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.message).from(res)
-        }
+        const attribute = await AttributeController.attributeService.delete(req.params.attributeId)
+        new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute deleted succesfully!").from(res)
     }
 
     private async getAllAttribute(req: Request, res: Response) {
-        try {
-            const attributes = await AttributeController.attributeService.getAll()
-            new SuccessfulResponse(attributes, HttpStatusCode.OK, "Attribute retrieved succesfully!").from(res)
-        } catch (e: any) {
-            new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.message).from(res)
-        }
+        const attributes = await AttributeController.attributeService.getAll()
+        new SuccessfulResponse(attributes, HttpStatusCode.OK, "Attribute retrieved succesfully!").from(res)
     }
 
     private async getAttribute(req: Request, res: Response) {
-        try {
-            const attribute = await AttributeController.attributeService.getById(req.params.attributeId)
-            new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute retrieved succesfully!").from(res)
-        } catch (e: any) {
-            new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.message).from(res)
-        }
+        const attribute = await AttributeController.attributeService.getById(req.params.attributeId)
+        new SuccessfulResponse(attribute, HttpStatusCode.OK, "Attribute retrieved succesfully!").from(res)
     }
 
     getPath(): string {
