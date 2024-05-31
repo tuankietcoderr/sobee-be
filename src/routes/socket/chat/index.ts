2b7777f033bdc9cdfff8e597346c47bf148c4dbe
Le Duy <duylangtu931@gmail.com>
2024-05-31 15:32:08 +0700

    fix: remove unused sendChatMessage function

--- src/routes/socket/chat/index.ts
+++ src/routes/socket/chat/index.ts
@@ -1,13 +1,11 @@
 import { Socket } from "socket.io"
 import createChatRoom from "./createChatRoom"
 import createChatMessage from "./createChatMessage"
-import sendChatMessage from "./sendChatMessage"
 import { viewListRoomChat, viewRoomChat } from "./viewChat"
 
 export default function handleChat(socket: Socket) {
   createChatRoom(socket)
   createChatMessage(socket)
-  sendChatMessage(socket)
   viewListRoomChat(socket)
   viewRoomChat(socket)
 }
