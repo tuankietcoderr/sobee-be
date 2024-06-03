952713ed1c4051bb25387196eac53291b81aac13
Le Duy <duylangtu931@gmail.com>
2024-06-03 11:43:10 +0700

    feat(category): Add parent and children functionality to CategoryService

--- src/common/utils/errorHandler.ts
+++ src/common/utils/errorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export const asyncHandler = (fn: Function) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err))
--- src/interface/schema/ICategory.ts
+++ src/interface/schema/ICategory.ts
@@ -1,7 +1,11 @@
+import { Types } from "mongoose"
+
 export interface ICategory {
   _id?: string
   name: string
   slug: string
+  parent: null | string | Types.ObjectId | ICategory
+  children: string[] | Types.ObjectId[] | ICategory[]
   description: string
   deletedAt: Date
   image: string
