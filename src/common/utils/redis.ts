8a7c9dfec3c986278e9dcbf2d1b16f1d65e5ca1f
tuankietcoder <charlieismylife2003@gmail.com>
2024-06-10 16:10:04 +0700

    add port to redis config

--- src/common/utils/redis.ts
+++ src/common/utils/redis.ts
@@ -3,7 +3,8 @@ import { createClient } from "redis"
 const redisClient = createClient({
   password: process.env.REDIS_PASSWORD,
   socket: {
-    host: process.env.REDIS_HOST
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT as string)
   }
 })
 
