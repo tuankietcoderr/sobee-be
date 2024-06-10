010b9bfbd9b2a964df7564d7cb9ef263403b35bd
tuankietcoder <charlieismylife2003@gmail.com>
2024-06-10 16:07:14 +0700

    add redis catch block

--- src/server.ts
+++ src/server.ts
@@ -84,7 +84,7 @@ async function start() {
   const message = await runSocketServer()
   console.log(message)
   await runHttpServer()
-  await redisClient.connect()
+  await redisClient.connect().catch((err) => console.log(err))
   getRoutes(app)
 }
 
